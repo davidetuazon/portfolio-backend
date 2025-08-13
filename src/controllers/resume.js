c2ac655d5edb481c627c16af494ed99bcd9dc6f1
davidetuazon <tuazonedavid@gmail.com>
2025-08-13 17:38:17 +0800

    fix resume path

--- src/controllers/resume.js
+++ src/controllers/resume.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 exports.resume = (req, res) => {
-    const filePath = path.join(__dirname, '../public', 'Resume_David_Tuazon.pdf');
+    const filePath = path.join(process.cwd(), '/public', 'Resume_David_Tuazon.pdf');
 
     res.type('application/pdf');
-
+    
     res.sendFile(filePath);
 };

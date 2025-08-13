5393990e69c656edd19dd3e4534713a295f4a1fa
davidetuazon <tuazonedavid@gmail.com>
2025-08-13 08:47:27 +0800

    update projects title

--- controllers/projects.js
+++ controllers/projects.js
@@ -2,27 +2,27 @@ exports.getProjects = (req, res) => {
     const projects = [
         {
             id: 1,
-            title: 'Qawah Cafe',
+            title: 'Qawah Cafe - E-Commerce Website',
             tech: ["Node.js", "Express", "Mongodb"]
         },
         {
             id: 2,
-            title: 'Undead Academia',
+            title: 'Undead Academia - Survival Horror Game',
             tech: ["Unity", "C#"]
         },
         {
             id: 3,
-            title: 'An Equal Share',
+            title: 'An Equal Share - GMTK Game Jam 2024',
             tech: ["Unity", "C#"]
         },
         {
             id: 4,
-            title: 'Handwritten Digit Recognition',
+            title: 'Handwritten Digit Recognition - Machine Learning Case study',
             tech: ["Python", "PyTorch"]
         },
         {
             id: 5,
-            title: 'Water Allocation Strategy using Genetic Algorithm',
+            title: 'Water Allocation Strategy Model using Genetic Algorithm',
             tech: ["Python", "DEAP"]
         }
     ];

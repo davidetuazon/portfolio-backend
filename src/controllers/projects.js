3182fa82cce8282b5ab5d805e8f321f2b54c9b3d
davidetuazon <tuazonedavid@gmail.com>
2025-08-14 09:38:41 +0800

    reshape project objects

--- src/controllers/projects.js
+++ src/controllers/projects.js
@@ -2,27 +2,37 @@ exports.getProjects = (req, res) => {
     const projects = [
         {
             id: 1,
-            title: 'Qawah Cafe - E-Commerce Website',
+            title: 'Qawah Cafe',
+            caption: 'E-Commerce Website',
+            description: '', 
             tech: ["Node.js", "Express", "Mongodb"]
         },
         {
             id: 2,
-            title: 'Undead Academia - Survival Horror Game',
+            title: 'Undead Academia',
+            caption: 'Survival Horror Game',
+            description: '', 
             tech: ["Unity", "C#"]
         },
         {
             id: 3,
-            title: 'An Equal Share - GMTK Game Jam 2024',
+            title: 'An Equal Share',
+            caption: 'GMTK Game Jam 2024',
+            description: '', 
             tech: ["Unity", "C#"]
         },
         {
             id: 4,
-            title: 'Handwritten Digit Recognition - Machine Learning Case study',
+            title: 'Handwritten Digit Recognition',
+            caption: 'Machine Learning Case study',
+            description: '', 
             tech: ["Python", "PyTorch"]
         },
         {
             id: 5,
-            title: 'Water Allocation Strategy Model using Genetic Algorithm',
+            title: 'Water Allocation Strategy Model',
+            caption: 'Model using Genetic Algorithm',
+            description: '', 
             tech: ["Python", "DEAP"]
         }
     ];
